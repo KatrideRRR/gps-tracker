cf0798a7daeaf65d29bc288b48a6e3436d2cb93f
Akim Adzhiametov <akimadzhiametov@gmail.com>
2023-02-16 21:48:39 +0300

    Fix review comments

--- sim_module_test_sketch/sim_module_test_sketch.ino
+++ sim_module_test_sketch/sim_module_test_sketch.ino
@@ -1,41 +1,39 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino D3 & D2
+//Create software serial object to communicate with SIM800L
+SoftwareSerial mySerial(3, 2);  //SIM800L Tx & Rx is connected to Arduino D3 & D2
 
-void setup()
-{
+void setup() {
+  //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
   
+  //Begin serial communication with Arduino and SIM800L
   mySerial.begin(9600);
 
-  Serial.println("Initializing..."); 
+  Serial.println("Initializing...");
   delay(1000);
 
-  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  mySerial.println("AT");  //Once the handshake test is successful, it will back to OK
   updateSerial();
 
-  mySerial.println("AT+CMGF=1"); //Configuring TEXT mode
+  mySerial.println("AT+CMGF=1");  //Configuring TEXT mode
   updateSerial();
-  mySerial.println("AT+CMGS=\"+0000000000\""); //Zeroes need to be replaced by phone number
+  mySerial.println("AT+CMGS=\"+0000000000\"");  //Zeroes need to be replaced by phone number
 
   updateSerial();
-  mySerial.print("Hello, world!!! From Sim module"); //Sms text content
+  mySerial.print("Hello, world!!! From Sim module");  //Sms text content
   updateSerial();
-  mySerial.write(26); //Operation termination
+  mySerial.write(26);  //Operation termination
 }
 
-void loop()
-{
+void loop() {
 }
 
-void updateSerial()
-{
+void updateSerial() {
   delay(500);
-  while (Serial.available()) 
-  {
-    mySerial.write(Serial.read()); //Forward what Serial received to Software Serial Port
+  while (Serial.available()) {
+    mySerial.write(Serial.read());  //Forward what Serial received to Software Serial Port
   }
-  while(mySerial.available()) 
-  {
-    Serial.write(mySerial.read()); //Forward what Software Serial received to Serial Port
+  while (mySerial.available()) {
+    Serial.write(mySerial.read());  //Forward what Software Serial received to Serial Port
   }
 }
